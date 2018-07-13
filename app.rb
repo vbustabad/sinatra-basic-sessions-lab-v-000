e53565750143bb80e4032f194954df70c75e4941
Veronica Bustabad <vbustabad@gmail.com>
2018-07-13 19:16:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+
+    @item = Item.new(params[:item])
+    @session[:item] = @item.name
+
+    erb :display
+  end
+
 end
